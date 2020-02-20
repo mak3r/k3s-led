eb6e94b454ab280c86ee23740834ef7f804ad4d3
Mark Abrams <mabrams@helpguest.com>
2020-02-20 01:59:11 +0000

    enable process termination

--- actions/app/blink1.sh
+++ actions/app/blink1.sh
@@ -6,8 +6,16 @@ gpio mode $pin out
 gpio mode $pin down
 
 while true; do
+  if test -f "./terminate"; then
+    echo "message to terminate received"
+    break
+  fi
   gpio write $pin 1
   sleep .0001
   gpio write $pin 0
   sleep 1
 done
+
+echo "Setting pin $pin low and terminating"
+# set the pin low (led off)
+gpio write $pin 0
--- actions/app/blink2.sh
+++ actions/app/blink2.sh
@@ -6,6 +6,10 @@ gpio mode $pin out
 gpio mode $pin down
 
 while true; do
+  if test -f "./terminate"; then
+    echo "message to terminate received"
+    break
+  fi
   for i in {1..3}; do
     gpio write $pin 1
     sleep .1
@@ -14,3 +18,7 @@ while true; do
   done
   sleep 1.5
 done
+
+echo "Setting pin $pin low and terminating"
+# set pin $pin low and terminate
+gpio write $pin 0
